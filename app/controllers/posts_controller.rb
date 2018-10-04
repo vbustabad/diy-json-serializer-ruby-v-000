38e60474770f2e9438a06c798f29b28384b95889
Veronica Bustabad <vbustabad@gmail.com>
2018-10-04 23:55:09 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
 
-  def body
+  def post_data
     post = Post.find(params[:id])
     render json: PostSerializer.serialize(post)
   end
